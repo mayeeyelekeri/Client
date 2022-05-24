afd077d670211005c1f6f869a5428d85b2273f2d
Yelekeri <yelekeri@gmail.com>
2022-05-23 20:37:39 -0400

    info-api path

---
+++ src/main/java/com/mine/demo/controller/TestController.java
@@ -0,0 +1,51 @@
+package com.mine.demo.controller;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mine.SpringDataTest.Model.FullName;
+import com.mine.SpringDataTest.Model.Problem;
+
+import net.minidev.json.JSONObject;
+
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+	private Logger logger = LoggerFactory.getLogger(TestController.class);
+	
+	@GetMapping()
+	public String test() {
+		logger.info("inside TestController().test()");
+		
+        return "test"; 
+	}
+}
